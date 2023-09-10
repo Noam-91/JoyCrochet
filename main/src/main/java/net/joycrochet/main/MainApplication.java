faee2c3edc304f815ffcaf9719a0c32f0adeec2f
Noam Yan <xuy3@illinois.edu>
2023-09-10 00:52:52 -0500

    First Commit

---
+++ main/src/main/java/net/joycrochet/main/MainApplication.java
@@ -0,0 +1,14 @@
+package net.joycrochet.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MainApplication {
+    public static void main(String[] args){
+        SpringApplication.run(MainApplication.class,args);
+    }
+
+}
